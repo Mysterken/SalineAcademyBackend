b26dc8a21780bfbc4b316be1eef4cbc4d6d66f4e
Mysterken <Mysterkenzo@gmail.com>
2023-09-14 12:39:52 +0200

    add check in the db for the test

--- tests/Command/CreateUserCommandTest.php
+++ tests/Command/CreateUserCommandTest.php
@@ -2,16 +2,25 @@
 
 namespace App\Tests\Command;
 
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 
-final class CreateUserCommandTest extends KernelTestCase
+class CreateUserCommandTest extends KernelTestCase
 {
+    private ObjectManager $entityManager;
+    private UserPasswordHasher $userPasswordHasher;
+
+    /**
+     * @throws Exception
+     */
     public function testExecute(): void
     {
-        $kernel = self::bootKernel();
-        $application = new Application($kernel);
+        $application = new Application(self::$kernel);
 
         $command = $application->find('app:create-user');
         $commandTester = new CommandTester($command);
@@ -22,9 +31,30 @@ final class CreateUserCommandTest extends KernelTestCase
         ]);
 
         $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Email: phpTestUsername@email.com', $output);
         $this->assertStringContainsString('Username: phpTestUsername', $output);
         $this->assertStringContainsString('Password: phpTestPassword', $output);
 
+        preg_match('/Id: (\d+)/', $output, $matches);
+        $id = $matches[1];
+
+        $user = $this->entityManager->getRepository(User::class)->find($id);
+        $this->assertNotNull($user);
+        $this->assertSame('phpTestUsername', $user->getUsername());
+        $this->assertSame('phpTestUsername@email.com', $user->getEmail());
+        $this->assertTrue($this->userPasswordHasher->isPasswordValid($user, 'phpTestPassword'));
+
         $commandTester->assertCommandIsSuccessful();
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        self::bootKernel(['environment' => 'test', 'debug' => false]);
+
+        $this->entityManager = self::$kernel->getContainer()->get('doctrine')->getManager();
+        $this->userPasswordHasher = self::$kernel->getContainer()->get('test.password_hasher');
     }
 }
